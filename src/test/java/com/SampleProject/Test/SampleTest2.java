06011858816b85df3fcca00514bb2715b05e8373
kmkanta <kandulamanikanta7@gmail.com>
2018-03-11 07:19:07 +0530

    Add new Test class file

---
+++ src/test/java/com/SampleProject/Test/SampleTest2.java
@@ -0,0 +1,13 @@
+package com.SampleProject.Test;
+
+import org.testng.annotations.BeforeClass;
+
+import com.SampleProject.TestBase.TestBase;
+
+public class SampleTest2 extends TestBase{
+	@BeforeClass
+	public void launch() {
+		
+	}
+
+}
